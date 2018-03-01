2d527560b39e0b7fce9ea38bd71bd3fdb653d712
João Guimarães <jccguimaraes@gmail.com>
2018-03-01 20:46:56 +0000

    refactor: Switch unit tests to mocha

--- spec/package-spec.js
+++ spec/package-spec.js
@@ -3,49 +3,36 @@
 const path = require('path');
 const sinon = require('sinon');
 
+const ProjectViewerPlus = require('./../bundle/main');
 const { PLUGIN_NAME } = require('./../bundle/constants/base');
+let sandbox;
 
 describe('package', function () {
 
   // START - SHOULD BE IN EVERY TEST SUIT
   before(function () {
-    this.sandbox = sinon.createSandbox();
+    sandbox = sinon.createSandbox();
+    return atom.packages.activatePackage('file-icons').then(() => true);
+  });
+
+  before(function () {
+    sandbox = sinon.createSandbox();
   });
 
   beforeEach(function () {
-    this.sandbox.stub(atom, 'getConfigDirPath')
-      .returns(path.join(__dirname, 'fixtures'));
+    const config = ProjectViewerPlus.config;
+    config.database.localPath = path.join(__dirname, 'fixtures');
+    return atom.config.set(PLUGIN_NAME, config);
   });
 
   afterEach(function () {
-    this.sandbox.restore();
+    sandbox.restore();
   });
   // STOP - SHOULD BE IN EVERY TEST SUIT
 
-  describe('when initialized with default config', function () {
-    beforeEach(function () {
-      return atom.packages.activatePackage(PLUGIN_NAME).then(() => {
-        return true;
-      });
-    });
-
-    afterEach(function () {
-      return atom.packages.deactivatePackage(PLUGIN_NAME).then(() => {
-        return true;
-      });
-    });
-
-    it('should have activated', function () {
-      expect(atom.packages.isPackageActive(PLUGIN_NAME)).to.be.true;
-    });
-
-    it('should be opened in the right dock as default', function () {
-      const panels = atom.workspace.getRightDock().getPaneItems();
-      const pkg = atom.packages.getActivePackage(PLUGIN_NAME);
-
-      expect(panels.length).to.equal(1);
-      expect(panels[0]).to.equal(pkg.mainModule.projectViewer);
-    });
+  it('when activated', function () {
+    ProjectViewerPlus.activate();
+    console.log(atom.config.get(PLUGIN_NAME));
   });
 
 });
