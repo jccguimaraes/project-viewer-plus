eb338284ba6dd76bea737a85637138ad2da45b31
João Guimarães <jccguimaraes@gmail.com>
2019-08-12 11:51:54 +0100

    test: begin specs improvement

--- spec/package-spec.js
+++ spec/package-spec.js
@@ -1,38 +1,65 @@
+'use strict';
+
 const path = require('path');
 const pvpPackage = path.resolve(__dirname, '..');
 
-let pane;
 let dock;
 let pkg;
 
+const databasePath = 'project-viewer-plus.database.localPath';
+const packageName = 'project-viewer-plus';
+const commandStart = `${packageName}:`;
+const uri = `atom://${packageName}:`;
+
 describe('package lifecycle', () => {
-  before(async () => {
-    attachToDOM(atom.workspace.getElement());
-    pane = atom.workspace.getActivePane();
-    dock = atom.workspace.getRightDock();
-    pkg = await atom.packages.loadPackage(pvpPackage);
-    // attachToDOM(atom.workspace.getElement());
+  before('set config database file path', () => {
+    atom.config.set(databasePath, path.resolve(__dirname, './mocks'));
   });
 
-  it('should attach to the right dock', async () => {
+  beforeEach('Activating package', async () => {
+    attachToDOM(atom.views.getView(atom.workspace));
+    dock = atom.workspace.getRightDock();
+    return atom.packages.activatePackage(pvpPackage);
+  });
+
+  beforeEach('get package', () => {
+    pkg = atom.packages.getLoadedPackage(packageName);
+  });
+
+  it('should have all commands available', () => {
+    const list = atom.commands
+      .findCommands({ target: atom.views.getView(atom.workspace) })
+      .filter(command => command.name.startsWith(commandStart));
+
+    expect(list.length).to.equal(12);
+  });
+
+  it('should not open the view on new Atom instance', async () => {
+    expect(dock.getPaneItems().length).to.equal(0);
+  });
+
+  it('should open the view', done => {
     dock.onDidAddPaneItem(
       pane => {
-        expect(pane.item).to.eql(pkg.mainModule.mainContainer);
         expect(dock.getPaneItems().length).to.equal(1);
+        const paneItem = dock.getPaneItems()[0];
+        expect(paneItem).to.eql(pkg.mainModule.mainContainer);
+        done();
       }
     );
 
-    await pkg.activate();
+    atom.workspace.open('atom://project-viewer-plus');
   });
 
-  it('should dettach from the right dock', async () => {
+  it('should dettach from the right dock', done => {
     dock.onDidDestroyPaneItem(
       pane => {
-        expect(pane.item).to.eql(pkg.mainModule.mainContainer);
         expect(dock.getPaneItems().length).to.equal(0);
+        expect(pane.item).to.eql(pkg.mainModule.mainContainer);
+        done();
       }
     );
 
-    await pkg.deactivate();
+    atom.packages.deactivatePackage(packageName);
   });
 });
